8d962ca7b50079f759a2b7c497e7c481aeb68ad4
raoulsullivan <stephen@apertureconsulting.co.uk>
2014-11-21 11:52:47 +0000

    Fixed file naming

--- markdown-to-pdf.py
+++ markdown-to-pdf.py
@@ -1,4 +1,4 @@
-"""Converts markdown to html and css"""
+"""Converts markdown to html and pdf"""
 import markdown
 import argparse
 import pdfkit
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     """Import arguments, run the thing"""
     parser = argparse.ArgumentParser(description='Convert .md to .html')
     parser.add_argument('input',type=str,help='Input .md file')
-    parser.add_argument('output',type=str,help='Output .html file')
+    parser.add_argument('output',type=str,help='Output .html file - will also do a pdf file of same name')
     arguments = vars(parser.parse_args())
     markdown.markdownFromFile(input=arguments['input'],output=arguments['output'])
     pdffile = arguments['output'].split('.')[0] + ".pdf"
